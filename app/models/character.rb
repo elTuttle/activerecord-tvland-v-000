399ef6b014699d7863460139c9917db99c388800
John Tuttle <thejwt@gmail.com>
2017-10-24 18:21:15 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,10 +2,13 @@ class Character < ActiveRecord::Base
   belongs_to :show
   belongs_to :actor
 
+  attr_accessor :catchphrase
+
   def actor=(actor)
     @actor = actor
     actor.characters << self
   end
   #binding.pry
+  
 
 end

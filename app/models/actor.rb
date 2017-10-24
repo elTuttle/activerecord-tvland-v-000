1da16b121a6bb33a5b1baf05be8868212b234e28
John Tuttle <thejwt@gmail.com>
2017-10-24 18:04:36 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,8 +5,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   #binding.pry
-  attr_accessor :characters
-  @characters = []
 
   def full_name
     "#{self.first_name} #{self.last_name}"
